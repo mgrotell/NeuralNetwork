e311299935899ecdc7dc698efc83e16f7df20eaf
Miklas Siivonen <msiivone@student.hive.fi>
2021-11-12 15:58:52 +0200

    some basic stuff

---
+++ headers/Neuralnetwork.h
@@ -0,0 +1,30 @@
+#ifndef NEURAL_NETWORK_H
+# define NEURAL_NETWORK_H
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <fcntl.h>
+
+# define WIN_W 1024
+# define WIN_H 768
+
+# define THREADS 64
+
+typedef struct s_neuralnetwork
+{
+        int     layers;
+        int     *layernodes;
+        double  learningrate;
+        double  ***weights;
+        double  ***biases;
+
+}               t_neuralnetwork;
+
+
+
+
+
+
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,15 +1,31 @@
+#include "../headers/neuralnetwork.h"
 
-
-
-
-
-
-
-int main(int argc, char *argv)
+int initializeNetwork(neuralnetwork_t *nn, char *settings)
 {
+	int x = 0;
+	
 
 
 
+}
+
+
+
+int	main(int ac, char *av)
+{
+	neuralnetwork_t *nn;
+
+	if(ac < 4)
+	{
+		ft_putstr("./nn 7 5 5 2 0.1 2");
+		ft_putstr("./nn firstlayer hiddenlayers... outputlayer learningrate activation(1-3 sigmoid, ReLU, LeakyReLu)");
+		return(0);
+	}
+	if(!(nn = (neuralnetwork_t*)malloc(sizeof(neuralnetwork_t))) || initializeNetwork(*nn, *settings))
+	{
+		ft_putstr("Cannot initialize network");
+		return (0);
+	}
 
 	return(0);
 }
